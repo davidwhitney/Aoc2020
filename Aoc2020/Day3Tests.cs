088e4177d4e0ec77f97e874728f67758d472fd9e
David Whitney <david@davidwhitney.co.uk>
2020-12-04 10:04:09 +0000

    day 3 p2

--- Aoc2020/Day3Tests.cs
+++ Aoc2020/Day3Tests.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using NUnit.Framework;
 
 namespace Aoc2020
@@ -26,7 +28,7 @@ namespace Aoc2020
                 ".#..#...#.#",
             };
 
-            var result = CountTrees(terrain);
+            var result = CountTrees(terrain,3, 1);
 
             Assert.That(result, Is.EqualTo(7));
         }
@@ -36,16 +38,33 @@ namespace Aoc2020
         {
             var terrain = File.ReadAllLines("Day3Input.txt").Select(x=>x.Trim()).ToList();
 
-            var result = CountTrees(terrain);
+            var result = CountTrees(terrain, 3, 1);
 
             Assert.That(result, Is.EqualTo(262));
         }
 
-        public int CountTrees(List<string> terrain)
+        [Test]
+        public void Part2()
         {
-            var slopeAcross = 3;
-            var slopeDown = 1;
+            var terrain = File.ReadAllLines("Day3Input.txt").Select(x=>x.Trim()).ToList();
 
+            var slopes = new List<int[]>
+            {
+                new[] {1, 1},
+                new[] {3, 1},
+                new[] {5, 1},
+                new[] {7, 1},
+                new[] {1, 2},
+            };
+
+            var counts = slopes.Select(slope => CountTrees(terrain, slope[0], slope[1])).ToList();
+            var total = counts.Aggregate<int, long>(1, (current, count) => current * count);
+
+            Assert.That(total, Is.EqualTo(2698900776));
+        }
+
+        public int CountTrees(List<string> terrain, int slopeX, int slopeY)
+        {
             var currentY = 0;
             var currentX = 0;
 
@@ -59,8 +78,8 @@ namespace Aoc2020
                     trees++;
                 }
 
-                currentX += slopeAcross;
-                currentY += slopeDown;
+                currentX += slopeX;
+                currentY += slopeY;
 
                 currentX = currentX >= terrain[0].Length
                     ? currentX - terrain[0].Length
