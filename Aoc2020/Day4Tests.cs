b9bd002adb0c083fdf241b2fec35418f241d9d62
David Whitney <david@davidwhitney.co.uk>
2020-12-04 16:59:47 +0000

    Refactor part 1 to make room for part 2.

--- Aoc2020/Day4Tests.cs
+++ Aoc2020/Day4Tests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 using NUnit.Framework;
 
 namespace Aoc2020
@@ -12,7 +13,7 @@ namespace Aoc2020
         [Test]
         public void FromLine_PassportWithSingleField_CorrectNumberOfFieldsDetected()
         {
-            var singleLineWithOneEntry = "ecl:gry";
+            const string singleLineWithOneEntry = "ecl:gry";
             var passport = Passport.FromLine(singleLineWithOneEntry);
             Assert.That(passport.Fields.Count, Is.EqualTo(1));
         }
@@ -20,7 +21,7 @@ namespace Aoc2020
         [Test]
         public void FromLine_PassportWithSingleField_Parses()
         {
-            var singleLineWithOneEntry = "ecl:gry";
+            const string singleLineWithOneEntry = "ecl:gry";
             var passport = Passport.FromLine(singleLineWithOneEntry);
             Assert.That(passport.Fields["ecl"], Is.EqualTo("gry"));
         }
@@ -28,7 +29,7 @@ namespace Aoc2020
         [Test]
         public void FromLine_PassportWithMultipleFields_Parses()
         {
-            var multipleValues = "ecl:gry pid:860033327";
+            const string multipleValues = "ecl:gry pid:860033327";
             var passport = Passport.FromLine(multipleValues);
             Assert.That(passport.Fields.Count, Is.EqualTo(2));
         }
@@ -36,7 +37,8 @@ namespace Aoc2020
         [Test]
         public void FromLine_PassportWithMultipleFieldsWithNewLineSplitter_Parses()
         {
-            var multipleValues = "ecl:gry\npid:860033327";
+            string multipleValues;
+            multipleValues = "ecl:gry\npid:860033327";
             var passport = Passport.FromLine(multipleValues);
             Assert.That(passport.Fields.Count, Is.EqualTo(2));
         }
@@ -44,7 +46,7 @@ namespace Aoc2020
         [Test]
         public void FromFile_GivenOneEntry_Parses()
         {
-            var singleLineWithOneEntry = "ecl:gry";
+            const string singleLineWithOneEntry = "ecl:gry";
             var passport = Passport.FromFile(singleLineWithOneEntry);
 
             Assert.That(passport.Count(), Is.EqualTo(1));
@@ -53,10 +55,10 @@ namespace Aoc2020
         [Test]
         public void FromFile_GivenMultipleEntry_Parses()
         {
-            var fileWithTwoEntries = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\r\n" +
-                                         "byr:1937 iyr:2017 cid:147 hgt:183cm\r\n\r\n" +
-                                         "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\r\n" +
-                                         "hcl:#cfa07d byr:1929";
+            const string fileWithTwoEntries = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\r\n" +
+                                              "byr:1937 iyr:2017 cid:147 hgt:183cm\r\n\r\n" +
+                                              "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\r\n" +
+                                              "hcl:#cfa07d byr:1929";
 
             var passport = Passport.FromFile(fileWithTwoEntries);
 
@@ -66,7 +68,7 @@ namespace Aoc2020
         [Test]
         public void Example1_ContainsAllFields()
         {
-            var input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\r\nbyr:1937 iyr:2017 cid:147 hgt:183cm\r\n";
+            const string input = "ecl:gry pid:860033327 eyr:2020 hcl:#fffffd\r\nbyr:1937 iyr:2017 cid:147 hgt:183cm\r\n";
             var passport = Passport.FromFile(input).First();
 
             Assert.That(passport.IsValid, Is.True);
@@ -75,7 +77,7 @@ namespace Aoc2020
         [Test]
         public void Example2_MissingHgt()
         {
-            var input = "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\r\nhcl:#cfa07d byr:1929";
+            const string input = "iyr:2013 ecl:amb cid:350 eyr:2023 pid:028048884\r\nhcl:#cfa07d byr:1929";
             var passport = Passport.FromFile(input).First();
 
             Assert.That(passport.IsValid, Is.False);
@@ -84,7 +86,7 @@ namespace Aoc2020
         [Test]
         public void Example2_OnlyMissingCID()
         {
-            var input = "hcl:#ae17e1 iyr:2013\r\neyr:2024\r\necl:brn pid:760753108 byr:1931\r\nhgt:179cm";
+            const string input = "hcl:#ae17e1 iyr:2013\r\neyr:2024\r\necl:brn pid:760753108 byr:1931\r\nhgt:179cm";
             var passport = Passport.FromFile(input).First();
 
             Assert.That(passport.IsValid, Is.True);
@@ -105,42 +107,25 @@ namespace Aoc2020
     public class Passport
     {
         public Dictionary<string, string> Fields { get; }
-        private readonly string[] _required;
+        private readonly List<Rule> _validators;
 
         public Passport(IEnumerable<KeyValuePair<string, string>> pairs)
         {
             Fields = new Dictionary<string, string>(pairs);
-            _required = new string[]
+            _validators = new List<Rule>
             {
-                "byr",
-                "iyr",
-                "eyr",
-                "hgt",
-                "hcl",
-                "ecl",
-                "pid",
-                "cid"
+                new("byr", (t) => { return true; }),
+                new("iyr", (t) => { return true; }),
+                new("eyr", (t) => { return true; }),
+                new("hgt", (t) => { return true; }),
+                new("hcl", (t) => { return true; }),
+                new("ecl", (t) => { return true; }),
+                new("pid", (t) => { return true; }),
+                new("cid", (t) => { return true; }, true)
             };
         }
 
-        public bool IsValid
-        {
-            get
-            {
-                var presentKeys = Fields.Keys.Intersect(_required).ToList();
-                if (presentKeys.Count == _required.Length)
-                {
-                    return true;
-                }
-
-                if (presentKeys.Count == 7 && !presentKeys.Contains("cid"))
-                {
-                    return true;
-                }
-
-                return false;
-            }
-        }
+        public bool IsValid => _validators.All(v => v.Validate(Fields));
 
         public static IEnumerable<Passport> FromFile(string passportFile)
         {
@@ -157,6 +142,35 @@ namespace Aoc2020
                 .ToDictionary(parts => parts[0], parts => parts[1]);
 
             return new Passport(dic);
+        }
+    }
+
+    public class Rule
+    {
+        public string FieldName { get; }
+        public Func<Dictionary<string, string>, bool> Validator { get; }
+        public bool Optional { get; }
+
+        public Rule(string fieldName, Func<Dictionary<string, string>, bool> validator, bool optional = false)
+        {
+            FieldName = fieldName;
+            Validator = validator;
+            Optional = optional;
+        }
+
+        public bool Validate(Dictionary<string, string> allFields)
+        {
+            if (Optional && !allFields.ContainsKey(FieldName))
+            {
+                return true;
+            }
+
+            if (!allFields.ContainsKey(FieldName))
+            {
+                return false;
+            }
+
+            return Validator(allFields);
         }
     }
 }
