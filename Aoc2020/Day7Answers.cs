3fb62e6828de36b749b28a2488bd9e661a7cdcb3
David Whitney <david@davidwhitney.co.uk>
2020-12-07 17:18:16 +0000

    Day 7 part 1

---
+++ Aoc2020/Day7Answers.cs
@@ -0,0 +1,95 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Aoc2020
+{
+    public class Day7Answers
+    {
+        public static Policy Parse(string[] rules)
+        {
+            return new(rules.Select(Parse).ToDictionary(x => x.Key, x => x.Value));
+        }
+
+        public static KeyValuePair<string, List<Requirement>> Parse(string rule)
+        {
+            const string keyToRuleDelimiter = "bags contain";
+            const string noRequirements = "no other bags";
+
+            var parts = rule.Split(keyToRuleDelimiter).Select(x => x.Trim()).ToList();
+            
+            var key = parts[0];
+            var requirementString = parts[1].Trim().Trim('.').ToLower();
+
+            if (requirementString == noRequirements)
+            {
+                return Requirement.None(key);
+            }
+            
+            var requirementsPreParsed = requirementString.Split(",").Select(x => x.Trim());
+            var ruleMatcher = new Regex("^([0-9]+) (.+) bag(s)?$");
+            var requirements = new List<Requirement>();
+
+            foreach (var req in requirementsPreParsed)
+            {
+                var parsed = ruleMatcher.Match(req);
+                var count = parsed.Groups[1].Value;
+                var type = parsed.Groups[2].Value;
+                int.TryParse(count, out var minimum);
+
+                var requirement = new Requirement(type, minimum);
+                requirements.Add(requirement);
+            }
+
+            return new KeyValuePair<string, List<Requirement>>(key, requirements);
+        }
+    }
+
+    public class Policy : Dictionary<string, List<Requirement>>
+    {
+        public Policy(IEnumerable<KeyValuePair<string, List<Requirement>>> items = null)
+        {
+            items ??= new List<KeyValuePair<string, List<Requirement>>>();
+            foreach (var (key, value) in items)
+            {
+                Add(key, value);
+            }
+        }
+
+        public int CountCanContain(string target)
+        {
+            var supports = 0;
+            foreach (var (key, _) in this)
+            {
+                var dependencies = AllRequirementsOf(key).Select(d => d.Target);
+                if (dependencies.Contains(target))
+                {
+                    supports++;
+                }
+            }
+
+            return supports;
+        }
+
+        public List<Requirement> AllRequirementsOf(string target, List<Requirement> all = null)
+        {
+            all ??= new List<Requirement>();
+
+            var requirements = this[target];
+            all.AddRange(requirements);
+
+            foreach (var requirement in requirements)
+            {
+                AllRequirementsOf(requirement.Target, all);
+            }
+
+            return all;
+        }
+    }
+
+    public record Requirement(string Target, int Minimum)
+    {
+        public static List<Requirement> Empty => new();
+        public static KeyValuePair<string, List<Requirement>> None(string forKey) => new(forKey, Empty);
+    }
+}
---
+++ Aoc2020/Day7Tests.cs
@@ -0,0 +1,110 @@
+using System.Diagnostics.Contracts;
+using System.IO;
+using NUnit.Framework;
+
+namespace Aoc2020
+{
+    [TestFixture]
+    public class Day7Tests
+    {
+        [Test]
+        public void Parse_RuleDependOnNoOtherBagTypes_ReturnsEmptyRequirements()
+        {
+            var result = Day7Answers.Parse("dotted black bags contain no other bags.");
+
+            Assert.That(result.Key, Is.EqualTo("dotted black"));
+            Assert.That(result.Value.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void Parse_RuleDependOnOneTypeOfBag_ReturnsOneRequirement()
+        {
+            var result = Day7Answers.Parse("light red bags contain 1 bright white bag.");
+
+            Assert.That(result.Key, Is.EqualTo("light red"));
+
+            Assert.That(result.Value[0].Target, Is.EqualTo("bright white"));
+            Assert.That(result.Value[0].Minimum, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Parse_RuleDependOnMultipleTypesOfBag_ReturnsOneRequirement()
+        {
+            var result = Day7Answers.Parse("light red bags contain 1 bright white bag, 2 muted yellow bags.");
+
+            Assert.That(result.Key, Is.EqualTo("light red"));
+
+            Assert.That(result.Value[0].Target, Is.EqualTo("bright white"));
+            Assert.That(result.Value[0].Minimum, Is.EqualTo(1));
+
+            Assert.That(result.Value[1].Target, Is.EqualTo("muted yellow"));
+            Assert.That(result.Value[1].Minimum, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Parse_MultipleRules_BothParsed()
+        {
+            var result = Day7Answers.Parse(new []
+            {
+                "dotted black bags contain no other bags.",
+                "light red bags contain 1 bright white bag."
+            });
+
+            Assert.That(result.Count, Is.EqualTo(2));
+        }
+    }
+
+    [TestFixture]
+    public class PolicyTests
+    {
+        private Policy _examplePolicy;
+        private string[] _exampleInput;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _exampleInput = new[]
+            {
+                "light red bags contain 1 bright white bag, 2 muted yellow bags.",
+                "dark orange bags contain 3 bright white bags, 4 muted yellow bags.",
+                "bright white bags contain 1 shiny gold bag.",
+                "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.",
+                "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.",
+                "dark olive bags contain 3 faded blue bags, 4 dotted black bags.",
+                "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.",
+                "faded blue bags contain no other bags.",
+                "dotted black bags contain no other bags."
+            };
+
+            _examplePolicy = Day7Answers.Parse(_exampleInput);
+        }
+
+        [Test]
+        public void Example1()
+        {
+            var result = _examplePolicy.CountCanContain("shiny gold");
+
+            Assert.That(result, Is.EqualTo(4));
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var inputs = File.ReadAllLines("Day7.txt");
+            var policy = Day7Answers.Parse(inputs);
+
+            var result = policy.CountCanContain("shiny gold");
+
+            Assert.That(result, Is.EqualTo(128));
+        }
+
+        [Test]
+        public void AllRequirementsOf()
+        {
+            var requirements = _examplePolicy.AllRequirementsOf("bright white");
+
+            Assert.That(requirements.Count, Is.EqualTo(7));
+            Assert.That(requirements[0].Target, Is.EqualTo("shiny gold"));
+        }
+    }
+}
